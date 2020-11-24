19d7be967a5e54d36056acfde3cb99106119b606
Brent Stone <brent.j.stone2.mil@mail.mil>
2020-11-24 16:42:03 -0500

    Lecture 32: Connect Functions & Buttons
    Added a basic function answerQuestion() and attached it to the onPressed method of each RaisedButton. The Debug Console shows the text printout from answerQuestion() when the buttons are pressed.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer Chosen!");
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -16,15 +20,15 @@ class MyApp extends StatelessWidget {
             Text("The Question!"),
             RaisedButton(
               child: Text("Answer 1"),
-              onPressed: null,
+              onPressed: answerQuestion, // no () to pass pointer to function
             ),
             RaisedButton(
               child: Text("Answer 2"),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text("Answer 3"),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
           ],
         ),
